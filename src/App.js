8edfee2f05cfff780814058bb30a4ff2a04e9e50
joedillonuk <joe.dillon.uk@gmail.com>
2021-08-19 16:40:54 +0100

    Seach functionality in place

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
 import github from "./db.js";
 import {useEffect, useState, useCallback } from "react";
 import query from "./Query";
+import RepoInfo from "./RepoInfo";
+import SearchBox from "./SearchBox";
+
 
 
 
 function App() {
 
   let [userName, setUserName] = useState("");
+  let [repoList, setRepoList] = useState(null);
+  let [pageCount, setPageCount] = useState(10);
+  let [queryString, setQueryString] = useState("");
+  let [totalCount, setTotalCount] = useState(null);
+
+
   const fetchData = useCallback( () => {
+    const queryText = JSON.stringify(query(pageCount, queryString));
+
+
+
     fetch(github.baseURL, {
       method: "POST",
       headers: github.headers,
-      body: JSON.stringify(query)
+      body: queryText
     })
     .then(response => response.json())
     .then(data => {
-      setUserName(data.data.viewer.name)
+      const viewer = data.data.viewer;
+      const repos = data.data.search.nodes;
+      const total = data.data.search.repositoryCount;
+      setUserName(viewer.name);
+      setRepoList(repos);
+      setTotalCount(total);
       console.log(data)
     })
     .catch( err => {
       console.log(err);
     })
-  }, [])
+  }, [pageCount, queryString])
 
   useEffect(() => {
     fetchData();
@@ -33,6 +51,29 @@ function App() {
         <i className="bi bi-diagram-2-fill"></i>Repos
       </h1>
       <p>Hey there {userName}</p>
+
+      <SearchBox
+        totalCount={totalCount}
+        pageCount={pageCount}
+        queryString={queryString}
+        onTotalChange={(myNumber) => {
+          setPageCount(myNumber);
+        }}
+        onQueryChange={(myString) => {
+          setQueryString(myString);
+        }}
+        />
+
+      
+      { repoList && (
+        <ul className="list group list-group-flush">
+          {repoList.map((repo) => (
+            <RepoInfo key={repo.id} repo={repo}/>
+              
+            ))}
+        </ul>
+      )}
+
     </div>
   );
 }
--- src/Query.js
+++ src/Query.js
@@ -1,11 +1,29 @@
-const githubQuery = {
+const githubQuery = (pageCount, queryString) => {
+    return {
     query: `
     {
-      viewer {
-        name
+        viewer {
+          name
+        }
+        search(query: "${queryString} user:joedillonuk sort:updated-desc", type: REPOSITORY, first: ${pageCount}) {
+            repositoryCount
+          nodes {
+            ... on Repository {
+              name
+              description
+              id
+              url
+              viewerSubscription
+              licenseInfo {
+                spdxId
+              }
+            }
+          }
+        }
       }
-    }
+  
     `,
   };
+}
 
   export default githubQuery;
