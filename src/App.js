6d4192af180efdc8c7c867d981798bedad3ef38d
joedillonuk <joe.dillon.uk@gmail.com>
2021-08-19 13:51:04 +0100

    Callback hook added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import github from "./db.js";
+import {useEffect, useState, useCallback } from "react";
+import query from "./Query";
+
+
 
 function App() {
+
+  let [userName, setUserName] = useState("");
+  const fetchData = useCallback( () => {
+    fetch(github.baseURL, {
+      method: "POST",
+      headers: github.headers,
+      body: JSON.stringify(query)
+    })
+    .then(response => response.json())
+    .then(data => {
+      setUserName(data.data.viewer.name)
+      console.log(data)
+    })
+    .catch( err => {
+      console.log(err);
+    })
+  }, [])
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mt-5">
+      <h1 className="text-primary">
+        <i className="bi bi-diagram-2-fill"></i>Repos
+      </h1>
+      <p>Hey there {userName}</p>
     </div>
   );
 }
