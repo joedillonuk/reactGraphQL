d16e33cb1bc1af30427bb500030df75c938ca19a
joedillonuk <joe.dillon.uk@gmail.com>
2021-08-19 19:32:53 +0100

    Pagination component added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {useEffect, useState, useCallback } from "react";
 import query from "./Query";
 import RepoInfo from "./RepoInfo";
 import SearchBox from "./SearchBox";
+import NavButtons from "./NavButtons";
 
 
 
@@ -15,11 +16,17 @@ function App() {
   let [queryString, setQueryString] = useState("");
   let [totalCount, setTotalCount] = useState(null);
 
+  let [startCursor, setStartCursor] = useState(null);
+  let [endCursor, setEndCursor] = useState(null);
+  let [hasPreviousPage, setHasPreviousPage] = useState(false);
+  let [hasNextPage, setHasNextPage] = useState(true);
+  let [paginationKeyword, setPaginationKeyword] = useState("first");
+  let [paginationString, setPaginationString] = useState("");
+
 
   const fetchData = useCallback( () => {
-    const queryText = JSON.stringify(query(pageCount, queryString));
-
-
+    const queryText = JSON.stringify(
+      query(pageCount, queryString, paginationKeyword, paginationString));
 
     fetch(github.baseURL, {
       method: "POST",
@@ -29,17 +36,27 @@ function App() {
     .then(response => response.json())
     .then(data => {
       const viewer = data.data.viewer;
-      const repos = data.data.search.nodes;
+      const repos = data.data.search.edges;
       const total = data.data.search.repositoryCount;
+      const start = data.data.search.pageInfo?.startCursor;
+      const end = data.data.search.pageInfo?.endCursor;
+      const next = data.data.search.pageInfo?.hasNextPage;
+      const prev = data.data.search.pageInfo?.hasPreviousPage;
+
       setUserName(viewer.name);
       setRepoList(repos);
       setTotalCount(total);
-      console.log(data)
+
+      setStartCursor(start);
+      setEndCursor(end);
+      setHasNextPage(next);
+      setHasPreviousPage(prev);
+
     })
     .catch( err => {
       console.log(err);
     })
-  }, [pageCount, queryString])
+  }, [pageCount, queryString, paginationString, paginationKeyword])
 
   useEffect(() => {
     fetchData();
@@ -64,15 +81,37 @@ function App() {
         }}
         />
 
-      
+      <NavButtons
+        start={startCursor}
+        end={endCursor}
+        next={hasNextPage}
+        previous={hasPreviousPage}
+        onPage={(myKeyword, myString) => {
+          setPaginationKeyword(myKeyword);
+          setPaginationString(myString);
+        }}
+
+      />
       { repoList && (
         <ul className="list group list-group-flush">
           {repoList.map((repo) => (
-            <RepoInfo key={repo.id} repo={repo}/>
+            <RepoInfo key={repo.node.id} repo={repo.node}/>
               
             ))}
         </ul>
       )}
+
+<NavButtons
+        start={startCursor}
+        end={endCursor}
+        next={hasNextPage}
+        previous={hasPreviousPage}
+        onPage={(myKeyword, myString) => {
+          setPaginationKeyword(myKeyword);
+          setPaginationString(myString);
+        }}
+
+      />
 
     </div>
   );
